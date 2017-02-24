91c671dbbd73c13ca3acea56f7dc6562380e40a4
amQel <martin.astim@gmail.com>
2017-02-24 11:47:42 +0100

    zadanie 1 maven + heap exception

---
+++ zad1D/zad1/src/main/java/com/ugJVMInernals/app/Ome.java
@@ -0,0 +1,13 @@
+package com.ugJVMInernals.app;
+
+import java.lang.*;
+
+public class Ome
+{
+  
+  public static void main(String[] args)
+  {
+    int i = 1239732621;
+    double[] p = new double[i];
+  }
+}
